e2aefc189fb9fead4267374115568c2bba06dcf2
brentgr@microsoft.com <brent.groom@live.com>
2023-02-10 13:04:44 -0700

    redis progress

--- src/controllers/AlbumController.js
+++ src/controllers/AlbumController.js
@@ -27,8 +27,8 @@ exports.orthopedic_surgeries_from_redis = async function (req, res) {
   // include azure log analytics
   //const logAnalytics = require("../utils/logAnalytics");
 
-  testCache();
-  console.log("Retrieved orthopedic surgeries from Redis ");
+  var redisresult = testCache();
+  console.log("Retrieved orthopedic surgeries from Redis "+redisresult);
   res.json(albums);
 };
 
@@ -45,37 +45,47 @@ async function testCache() {
     // Connect to the Azure Cache for Redis over the TLS port using the key.
     var cacheHostName = process.env.rediscachehostname;
     var cachePassword = process.env.rediscachekey;
-    var cacheConnection = redis.createClient({
-        // rediss for TLS
-        url: "rediss://" + cacheHostName + ":6380",
-        password: cachePassword,
-    });
-    await cacheConnection.connect();
+    console.log("\ncachePassword ***"+cachePassword+"*** "+cachePassword.length);
+    console.log("\ncacheHostName ***"+cacheHostName+"*** "+cacheHostName.length);
+    try { 
+        // Perform cache operations using the cache connection object...
+        var cacheConnection = redis.createClient({
+            // rediss for TLS
+            url: "rediss://" + cacheHostName + ":6380",
+            password: cachePassword,
+        });
+        await cacheConnection.connect();
+        console.log("\nCache command: PING");
+        console.log("Cache response : " + await cacheConnection.ping());
+        // Simple PING command
+        console.log("\nCache command: PING");
+        console.log("Cache response : " + await cacheConnection.ping());
+        // Simple get and put of integral data types into the cache
+        console.log("\nCache command: GET Message");
+        console.log("Cache response : " + await cacheConnection.get("Message"));
 
-    // Perform cache operations using the cache connection object...
+        console.log("\nCache command: SET Message");
+        console.log("Cache response : " + await cacheConnection.set("Message",
+            "Hello! The cache is working again from Node.js!"));
 
-    // Simple PING command
-    console.log("\nCache command: PING");
-    console.log("Cache response : " + await cacheConnection.ping());
+        // Demonstrate "SET Message" executed as expected...
+        console.log("\nCache command: GET Message");
+        console.log("Cache response : " + await cacheConnection.get("Message"));
 
-    // Simple get and put of integral data types into the cache
-    console.log("\nCache command: GET Message");
-    console.log("Cache response : " + await cacheConnection.get("Message"));
+        // Get the client list, useful to see if connection list is growing...
+        console.log("\nCache command: CLIENT LIST");
+        console.log("Cache response : " + await cacheConnection.sendCommand(["CLIENT", "LIST"]));
 
-    console.log("\nCache command: SET Message");
-    console.log("Cache response : " + await cacheConnection.set("Message",
-        "Hello! The cache is working again from Node.js!"));
+        console.log("\nDone");
+        process.exit();
+    }
+    catch (err) {
+        console.log("Error connecting to cache: " + err);
+    }
 
-    // Demonstrate "SET Message" executed as expected...
-    console.log("\nCache command: GET Message");
-    console.log("Cache response : " + await cacheConnection.get("Message"));
+    return "result from redis function"
 
-    // Get the client list, useful to see if connection list is growing...
-    console.log("\nCache command: CLIENT LIST");
-    console.log("Cache response : " + await cacheConnection.sendCommand(["CLIENT", "LIST"]));
 
-    console.log("\nDone");
-    process.exit();
 }
 
 
